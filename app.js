8487367699064813f9efaf920718ee7339466b5e
futariusagi <111503972+futariusagi@users.noreply.github.com>
2022-08-18 23:50:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('BTS Forever ARMY Forever!'));
+app.get('/', (req, res) => res.send('BTS Forever ARMY Forever! LOVE'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
